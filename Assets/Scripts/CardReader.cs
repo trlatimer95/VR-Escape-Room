d35bb8b15575da339efbea048b636658461e9fbf
jewelz ludwig <trlatimer95@gmail.com>
2022-10-26 22:03:21 -0700

    Implemented card swiping

--- Assets/Scripts/CardReader.cs
+++ Assets/Scripts/CardReader.cs
@@ -5,15 +5,53 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class CardReader : XRSocketInteractor
 {
-    // Start is called before the first frame update
-    void Start()
+    public float minSwipeDistance;
+    public float allowableUprightRange;
+    public GameObject lockToHide;
+    public XRGrabInteractable doorHandleInteractable;
+
+    private Transform keycardTransform;
+    private Vector3 cardEntryPosition;
+    private bool swipeIsValid;
+
+    private void Update()
     {
-        
+        if (keycardTransform != null)
+        {
+            Vector3 keycardUp = keycardTransform.forward;
+            float dot = Vector3.Dot(keycardUp, Vector3.up);
+            
+            if (dot < 1 - allowableUprightRange)
+                swipeIsValid = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnHoverEntered(HoverEnterEventArgs args)
     {
-        
+        base.OnHoverEntered(args);
+
+        keycardTransform = args.interactableObject.transform;
+        cardEntryPosition = keycardTransform.position;
+        swipeIsValid = true;
+    }
+
+    protected override void OnHoverExited(HoverExitEventArgs args)
+    {
+        base.OnHoverExited(args);
+
+        Vector3 swipeLength = args.interactableObject.transform.position - cardEntryPosition;
+        if (swipeIsValid && swipeLength.y < minSwipeDistance)
+        {
+            lockToHide.gameObject.SetActive(false);
+            doorHandleInteractable.enabled = true;
+        }
+
+        keycardTransform = null;
+    }
+
+    // Prevent snapping
+    public override bool CanSelect(IXRSelectInteractable interactable)
+    {
+        return false;
     }
 }
